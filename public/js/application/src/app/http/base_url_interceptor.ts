1227ad43c3f5838447eab5fa9163d0b071a6fc21
Andriy Haydash <angay9@gmail.com>
2017-12-26 11:51:35 +0100

    ADD api url

--- public/js/application/src/app/http/base_url_interceptor.ts
+++ public/js/application/src/app/http/base_url_interceptor.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 import { AuthService } from '../services/auth/auth.service';
 
@@ -14,7 +15,7 @@ export class BaseUrlInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        const apiReq = req.clone({ url: `http://angular-iread.local/api/${req.url}` });
+        const apiReq = req.clone({ url: `${environment.apiUrl}/${req.url}` });
 
         return next.handle(apiReq);
     }
--- public/js/application/src/environments/environment.prod.ts
+++ public/js/application/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://api-iread.demo-server.ovh/api'
 };
